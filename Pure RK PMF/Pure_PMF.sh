4f7fc75926db005e33392bf87513695478c6c077
Garren-H <garren382@gmail.com>
2024-04-29 21:27:10 +0200

    bash script to submit to server

---
+++ Pure RK PMF/Pure_PMF.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#PBS -l walltime=168:00:00
+#PBS -l select=1:ncpus=32:mem=5GB
+#PBS -m abe
+#PBS -o Pure_PMF.out
+#PBS -e Pure_PMF.err
+#PBS -N Pure_PMF
+
+# make sure I'm the only one that can read my output
+umask 0077
+
+cd Pure\ RK\ PMF
+
+module load app/stan/2.34
+
+python3 Pure_PMF.py ${include_clusters} ${variance_known} ${variance_MC_known}
+
+# usage for PBS: qsub -v include_clusters=0,variance_known=1,variance_MC_known=1 Pure_PMF.sh
